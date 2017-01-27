38070b5db157a4e04fc328ac617fa96e5b3dc2bd
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-27 00:32:26 -0500

    test gateway added

--- src/test/java/org/media/dal/TestGateway.java
+++ src/test/java/org/media/dal/TestGateway.java
@@ -9,5 +9,13 @@ import java.sql.Connection;
  * Created by shantonu on 1/27/17.
  */
 public class TestGateway {
+    private Connection aConnection;
 
+    @Before
+    public void initTest(){
+        Gateway.getConnection();
+    }
+    public void testConnection(){
+        Assert.assertNotNull(aConnection);
+    }
 }
