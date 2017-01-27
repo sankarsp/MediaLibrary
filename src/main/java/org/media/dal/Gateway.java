50011d467585f55937a99fcd24137ddf7065f53f
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-27 00:26:43 -0500

    connection adding

---
+++ src/main/java/org/media/dal/Gateway.java
@@ -0,0 +1,12 @@
+package org.media.dal;
+
+import java.sql.Connection;
+
+/**
+ * Created by shantonu on 1/27/17.
+ */
+public class Gateway {
+    public static Connection getConnection(){
+        return null;
+    }
+}
