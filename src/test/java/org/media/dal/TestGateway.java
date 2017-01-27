d709395e5357604368e580ac44a5e84c282bdc79
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-27 00:35:34 -0500

    connection test added

--- src/test/java/org/media/dal/TestGateway.java
+++ src/test/java/org/media/dal/TestGateway.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by shantonu on 1/27/17.
@@ -17,5 +18,10 @@ public class TestGateway {
     }
     public void testConnection(){
         Assert.assertNotNull(aConnection);
+        try {
+            Assert.assertFalse(aConnection.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
