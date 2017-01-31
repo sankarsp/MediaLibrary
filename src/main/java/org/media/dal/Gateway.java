3dc4da068135e4f8fb7c825f4f9cffa069f315ea
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-30 23:53:54 -0500

    MySQL support added

--- src/main/java/org/media/dal/Gateway.java
+++ src/main/java/org/media/dal/Gateway.java
@@ -1,6 +1,8 @@
 package org.media.dal;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by shantonu on 1/27/17.
@@ -10,10 +12,24 @@ public class Gateway {
     private static Connection aConnection = null;
     private Gateway(){}
 
-    public static Connection getConnection(){
+    public static Connection getConn(){
         if(aConnection==null){
     //create the connection from the DB
         }
         return aConnection;
+    }
+
+    public static String url= "jdbc:mysql://<HOST>:<PORT>/<DB>";
+    private static final String user = "user";
+    private static final String pass = "pass";
+    private static final String className = "com.mysql.jdbc.Driver";
+
+    public static synchronized Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        return DriverManager.getConnection(url, user, pass);
+    }
+    public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class.forName(className).newInstance();
+        return DriverManager.getConnection(url,user,pass);
     }
 }
