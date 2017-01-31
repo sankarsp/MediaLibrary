a33338e579dcc62c2e34f13694fb02c2beddab1a
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-31 00:59:02 -0500

    getting items from DB

--- src/main/java/org/media/dal/ArtistGateway.java
+++ src/main/java/org/media/dal/ArtistGateway.java
@@ -5,9 +5,10 @@ import java.sql.Connection;
 /**
  * Created by shantonu on 1/31/17.
  */
-public class ArtistGateway {
+public class ArtistGateway extends Gateway{
     private Connection aConnection;
     public ArtistGateway(Connection conneciton) {
+        super();
         this.aConnection = conneciton;
     }
 }
--- src/main/java/org/media/dal/Gateway.java
+++ src/main/java/org/media/dal/Gateway.java
@@ -1,6 +1,13 @@
 package org.media.dal;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by shantonu on 1/27/17.
@@ -8,8 +15,27 @@ import java.sql.*;
 public class Gateway {
 
     private static Connection aConnection = null;
-    private Gateway(){}
-
+    Gateway(){}
+    public static <T> List<T> createObjects(ResultSet resultSet, Class<T> tClass) throws
+            SQLException, IllegalAccessException, InstantiationException, IntrospectionException, InvocationTargetException {
+        List<T> listImtes = new ArrayList<T>();
+        while (resultSet.next()) {
+            T intance = tClass.newInstance();
+            for (Field field : tClass.getDeclaredFields()) {
+                PropertyDescriptor pd = new PropertyDescriptor(field.getName(), tClass);
+                //System.out.println("PD " + pd.getName());
+                Method method = pd.getWriteMethod();
+              /*  System.out.println("Method : " + method.getName());
+                System.out.println("Method : Parameter count " + method.getParameterCount());*/
+                Class[] types = method.getParameterTypes();
+                //System.out.println("Method : Parameter type "+types[0]);
+                Object value = resultSet.getObject(field.getName());
+                method.invoke(intance, (types[0]).cast(value));
+            }
+            listImtes.add(intance);
+        }
+        return listImtes;
+    }
     public static Connection getConn(){
         if(aConnection==null){
             try {
@@ -37,7 +63,7 @@ public class Gateway {
         Class.forName(className).newInstance();
         return DriverManager.getConnection(url,user,pass);
     }
-    public static ResultSet runQuerry(String query) throws SQLException {
+    protected ResultSet runQuerry(String query) throws SQLException {
         Statement pt = getConn().createStatement();
         return pt.executeQuery(query);
     }
