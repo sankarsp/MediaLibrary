81c674fc8386c06ebaa4ed936c9117b83d44c458
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-30 23:56:20 -0500

    Property loader added

--- src/main/java/org/media/App.java
+++ src/main/java/org/media/App.java
@@ -1,7 +1,24 @@
 package org.media;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by shantonu on 1/26/17.
  */
 public class App {
+
+    public static void loadProperties(){
+        Properties p = new Properties();
+        try {
+            p.load(new FileInputStream(new File("src/main/resources/app.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(String k:p.stringPropertyNames()){
+            System.setProperty(k,p.getProperty(k));
+        }
+    }
 }
