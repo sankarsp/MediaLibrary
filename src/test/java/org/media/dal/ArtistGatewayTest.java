e7898b771b06b7124e1008423f788989ccadfd6f
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-31 00:13:30 -0500

    tests

---
+++ src/test/java/org/media/dal/ArtistGatewayTest.java
@@ -0,0 +1,40 @@
+package org.media.dal;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.media.TestBase;
+
+/**
+ * Created by shantonu on 1/31/17.
+ */
+public class ArtistGatewayTest extends TestBase {
+
+    private Artist anArtist;
+    @Before
+    public void inittest(){
+        anArtist = new Artist();
+    }
+
+    @Test
+    public void testGetAnArtistFromDB(){
+
+    }
+
+    @Test
+    public void testGetAllArtistFromDB(){
+
+    }
+
+    @Test
+    public void testDeleteAnArtistFromDB(){
+
+    }
+    @Test
+    public void testUpdateAnArtistFromDB(){
+
+    }
+    @Test
+    public void testcreateAnArtistFromDB(){
+
+    }
+}
