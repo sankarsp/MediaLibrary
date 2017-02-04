183bdb0dd8d3e583e6223c9898ec2c19a694c76d
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-04 03:46:45 -0500

    reviewer complete

--- src/main/java/org/media/model/Reviewer.java
+++ src/main/java/org/media/model/Reviewer.java
@@ -4,5 +4,28 @@ package org.media.model;
  * Created by shantonu on 2/4/17.
  */
 public class Reviewer {
+    private Long id;
+    private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Reviewer(){}
+    public Reviewer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
