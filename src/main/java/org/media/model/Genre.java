bf323e0e4c6cac4b53e9a725cf6a3ef6cfe31621
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-04 03:47:33 -0500

    genre completed

--- src/main/java/org/media/model/Genre.java
+++ src/main/java/org/media/model/Genre.java
@@ -4,4 +4,28 @@ package org.media.model;
  * Created by shantonu on 2/4/17.
  */
 public class Genre {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Genre(){}
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
