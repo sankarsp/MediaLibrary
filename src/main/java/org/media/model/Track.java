5af8855f5b44d0e8dfcd2102c97f6322cbb92a39
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-04 03:50:00 -0500

    track complete

--- src/main/java/org/media/model/Track.java
+++ src/main/java/org/media/model/Track.java
@@ -1,7 +1,64 @@
 package org.media.model;
 
+import java.util.Date;
+
 /**
  * Created by shantonu on 2/4/17.
  */
 public class Track {
+    private Long id;
+    private String title;
+    private Date releaseDate;
+    private Long artistId;
+    private Long labelId;
+
+
+    public Track(){}
+    public Track(Long id, String title, Date releaseDate, Long artistId, Long labelId) {
+        this.id = id;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.artistId = artistId;
+        this.labelId = labelId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Long getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(Long artistId) {
+        this.artistId = artistId;
+    }
+
+    public Long getLabelId() {
+        return labelId;
+    }
+
+    public void setLabelId(Long labelId) {
+        this.labelId = labelId;
+    }
 }
