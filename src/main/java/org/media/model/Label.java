4d99aac6c605de29677d489849703bd6b9b7e674
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-04 03:45:25 -0500

    artist class complete

--- src/main/java/org/media/model/Label.java
+++ src/main/java/org/media/model/Label.java
@@ -4,4 +4,28 @@ package org.media.model;
  * Created by shantonu on 2/4/17.
  */
 public class Label {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Label(){}
+    public Label(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
