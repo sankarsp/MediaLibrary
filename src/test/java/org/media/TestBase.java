b63529cefa649b8c80c1685f8cccd2e4fc5a5bbb
shantonu sarker <shantonu_oxford@yahoo.com>
2017-02-06 00:11:29 -0500

    test base seperated

--- src/test/java/org/media/TestBase.java
+++ src/test/java/org/media/TestBase.java
@@ -1,7 +1,18 @@
 package org.media;
 
+import org.junit.BeforeClass;
+import org.media.dal.ConnectionManager;
+
+import java.sql.Connection;
+
 /**
  * Created by shantonu on 1/27/17.
  */
 public abstract class TestBase {
+    public static Connection aConnection;
+
+    @BeforeClass
+    public static void initTestClass(){
+        aConnection = ConnectionManager.getConn();
+    }
 }
