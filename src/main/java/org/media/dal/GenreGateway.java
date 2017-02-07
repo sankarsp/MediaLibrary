53f24fcbbf5d1889e7ed580c80672c27247c1db1
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-07 04:16:29 -0500

    genre gateway

--- src/main/java/org/media/dal/GenreGateway.java
+++ src/main/java/org/media/dal/GenreGateway.java
@@ -1,7 +1,16 @@
 package org.media.dal;
 
+import org.media.model.Genre;
+
+import java.util.List;
+
 /**
  * Created by ssarker on 2/5/2017.
  */
-public class GenreGateway {
+public class GenreGateway implements Gatewayable{
+
+
+    public  List<Genre> viewAll() {
+        return null;
+    }
 }
