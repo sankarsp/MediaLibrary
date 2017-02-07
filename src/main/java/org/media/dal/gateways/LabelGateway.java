c1981392ec71f2b0872a2161b8892226f1c23eb5
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-07 04:25:39 -0500

    label gateway

--- src/main/java/org/media/dal/gateways/LabelGateway.java
+++ src/main/java/org/media/dal/gateways/LabelGateway.java
@@ -1,7 +1,41 @@
 package org.media.dal.gateways;
 
+import org.media.core.Gatewayable;
+import org.media.model.Label;
+
+import java.util.List;
+
 /**
  * Created by ssarker on 2/5/2017.
  */
-public class LabelGateway {
+public class LabelGateway implements Gatewayable<Label>{
+    @Override
+    public List<Label> viewAll() {
+        return null;
+    }
+
+    @Override
+    public Label view(Long id) {
+        return null;
+    }
+
+    @Override
+    public Long Insert(Label label) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public void remove(Label label) {
+
+    }
+
+    @Override
+    public Long update(Label label) {
+        return null;
+    }
 }
