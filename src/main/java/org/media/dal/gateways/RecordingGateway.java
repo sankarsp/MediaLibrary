f00004e3c88ab1872bc562fe8d10897b2d9fcdf7
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-07 04:25:13 -0500

    recording gateway

--- src/main/java/org/media/dal/gateways/RecordingGateway.java
+++ src/main/java/org/media/dal/gateways/RecordingGateway.java
@@ -1,7 +1,41 @@
 package org.media.dal.gateways;
 
+import org.media.core.Gatewayable;
+import org.media.model.Recording;
+
+import java.util.List;
+
 /**
  * Created by ssarker on 2/5/2017.
  */
-public class RecordingGateway {
+public class RecordingGateway implements Gatewayable<Recording>{
+    @Override
+    public List<Recording> viewAll() {
+        return null;
+    }
+
+    @Override
+    public Recording view(Long id) {
+        return null;
+    }
+
+    @Override
+    public Long Insert(Recording recording) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public void remove(Recording recording) {
+
+    }
+
+    @Override
+    public Long update(Recording recording) {
+        return null;
+    }
 }
