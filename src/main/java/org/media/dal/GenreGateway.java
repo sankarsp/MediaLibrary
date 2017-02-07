d8d3dacec364f4790525c4d2fd249f1709b46898
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-07 04:20:13 -0500

    genre gateway => todo

--- src/main/java/org/media/dal/GenreGateway.java
+++ src/main/java/org/media/dal/GenreGateway.java
@@ -7,10 +7,36 @@ import java.util.List;
 /**
  * Created by ssarker on 2/5/2017.
  */
-public class GenreGateway implements Gatewayable{
+public class GenreGateway implements Gatewayable<Genre>{
 
 
-    public  List<Genre> viewAll() {
+    @Override
+    public List<Genre> viewAll() {
+        return null;
+    }
+
+    @Override
+    public Genre view(Long id) {
+        return null;
+    }
+
+    @Override
+    public Long Insert(Genre genre) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public void remove(Genre genre) {
+
+    }
+
+    @Override
+    public Long update(Genre genre) {
         return null;
     }
 }
