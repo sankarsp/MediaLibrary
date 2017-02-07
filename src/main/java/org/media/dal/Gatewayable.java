15216611533d746b610c07fe6d5249c2fe95f463
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-07 04:20:01 -0500

    common gateway interface using generics

--- src/main/java/org/media/dal/Gatewayable.java
+++ src/main/java/org/media/dal/Gatewayable.java
@@ -5,6 +5,17 @@ import java.util.List;
 /**
  * Created by shantonu on 2/7/17.
  */
-public interface Gatewayable {
-    <T> List<T> viewAll();
+public interface Gatewayable<T> {
+    List<T> viewAll();
+
+    T view(Long id);
+
+    Long Insert(T t);
+
+    void remove(Long id);
+
+    void remove(T t);
+
+    Long update(T t);
+
 }
