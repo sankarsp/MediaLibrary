ffe806ca1c49f6b9983ba153dd5c2ab1fbbc27f0
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-17 23:43:16 -0500

    get id using tableName

--- src/main/java/org/media/utils/IDGenerator.java
+++ src/main/java/org/media/utils/IDGenerator.java
@@ -4,4 +4,8 @@ package org.media.utils;
  * Created by shantonu on 2/17/17.
  */
 public class IDGenerator {
+    public static Integer giveMeNextId(String tableName){
+
+        return 1;
+    }
 }
