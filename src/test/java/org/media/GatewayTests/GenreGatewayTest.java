ad0d5510edb754d61c1f44a6512bb337c8ea8e6c
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-27 03:13:57 -0500

    testGetAllGenreFromDB

--- src/test/java/org/media/GatewayTests/GenreGatewayTest.java
+++ src/test/java/org/media/GatewayTests/GenreGatewayTest.java
@@ -1,8 +1,15 @@
 package org.media.GatewayTests;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.media.TestBase;
 import org.media.dal.gateways.GenreGateway;
+import org.media.model.Genre;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by shantonu on 2/25/17.
@@ -12,6 +19,16 @@ public class GenreGatewayTest extends TestBase{
     @Before
     public void init(){
         aGenreGateway = new GenreGateway();
+    }
+    @Test
+    public void testGetAllGenreFromDB(){
+        List<Genre> allGenre = aGenreGateway.viewAll();
+        assertTrue(allGenre.size()>0);
+        for(Genre Genre : allGenre){
+            Genre found = aGenreGateway.view(Genre.getID());
+            assertEquals(found.getID(), Genre.getID());
+            assertEquals(found.getNAME(), Genre.getNAME());
+        }
     }
 
 }
