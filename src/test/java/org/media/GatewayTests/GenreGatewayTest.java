2e5062a5a32092caacf86b48a68bf3a7cd52fab7
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-27 03:12:17 -0500

    init test

--- src/test/java/org/media/GatewayTests/GenreGatewayTest.java
+++ src/test/java/org/media/GatewayTests/GenreGatewayTest.java
@@ -1,7 +1,17 @@
 package org.media.GatewayTests;
 
+import org.junit.Before;
+import org.media.TestBase;
+import org.media.dal.gateways.GenreGateway;
+
 /**
  * Created by shantonu on 2/25/17.
  */
-public class GenreGatewayTest {
+public class GenreGatewayTest extends TestBase{
+    private GenreGateway aGenreGateway;
+    @Before
+    public void init(){
+        aGenreGateway = new GenreGateway();
+    }
+
 }
