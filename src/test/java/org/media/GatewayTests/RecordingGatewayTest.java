1a9173053f3d142f15a3ef0fba94da201915e5b1
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-01 01:47:12 -0500

    recording gateway added

--- src/test/java/org/media/GatewayTests/RecordingGatewayTest.java
+++ src/test/java/org/media/GatewayTests/RecordingGatewayTest.java
@@ -1,11 +1,19 @@
 package org.media.GatewayTests;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.media.dal.gateways.RecordingGateway;
 
 /**
  * Created by shantonu on 2/26/17.
  */
 public class RecordingGatewayTest {
+    private RecordingGateway aRecordingGateway;
+    @Before
+    public void init(){
+        aRecordingGateway = new RecordingGateway();
+    }
+
     @Test
     public void testGetARecordingFromDB(){
 
