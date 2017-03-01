01bb9720d051b465f3aad94720f273808510ec98
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-01 01:48:14 -0500

    label gateway gateway added

--- src/test/java/org/media/GatewayTests/LabelGatewayTest.java
+++ src/test/java/org/media/GatewayTests/LabelGatewayTest.java
@@ -1,11 +1,20 @@
 package org.media.GatewayTests;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.media.dal.gateways.LabelGateway;
+
 
 /**
  * Created by shantonu on 2/25/17.
  */
 public class LabelGatewayTest {
+    private LabelGateway aLabelGateway;
+    @Before
+    public void init(){
+        aLabelGateway = new LabelGateway();
+    }
+
     @Test
     public void testGetALabelFromDB(){
 
