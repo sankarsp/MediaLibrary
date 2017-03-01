b80cb40518f65ff7b2e2decfd2ec8b39c1c12ca4
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-28 23:57:21 -0500

    reviewer gateway test method

--- src/test/java/org/media/GatewayTests/ReviewerGatewayTest.java
+++ src/test/java/org/media/GatewayTests/ReviewerGatewayTest.java
@@ -1,7 +1,22 @@
 package org.media.GatewayTests;
 
+import org.junit.Test;
+
 /**
  * Created by shantonu on 2/27/17.
  */
 public class ReviewerGatewayTest {
+    @Test
+    public void testGetAReviewertFromDB(){
+
+    }
+    @Test
+    public void testGetAllReviewerFromDB(){}
+    @Test
+    public void testDeleteAReviewerFromDB(){}
+    @Test
+    public void testInsertAReviewerFromDB(){}
+    @Test
+    public void testUpdateAReviewerFromDB(){}
 }
+
