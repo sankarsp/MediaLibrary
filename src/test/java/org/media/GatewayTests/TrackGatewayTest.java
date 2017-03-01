ee796d9551ce340fbc49d703581f1713f4b088a4
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-01 01:45:09 -0500

    track gatewaya added

--- src/test/java/org/media/GatewayTests/TrackGatewayTest.java
+++ src/test/java/org/media/GatewayTests/TrackGatewayTest.java
@@ -1,11 +1,18 @@
 package org.media.GatewayTests;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.media.dal.gateways.TrackGateway;
 
 /**
  * Created by shantonu on 2/27/17.
  */
 public class TrackGatewayTest {
+    private TrackGateway aTrackGateway;
+    @Before
+    public void init(){
+        aTrackGateway = new TrackGateway();
+    }
     @Test
     public void testGetATrackFromDB() {
 
