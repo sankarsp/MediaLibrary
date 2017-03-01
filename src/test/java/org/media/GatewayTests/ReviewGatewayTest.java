b784395b4373378cd6272f7b560fb99ac1e007bc
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-28 23:58:02 -0500

    review  gateway test method

--- src/test/java/org/media/GatewayTests/ReviewGatewayTest.java
+++ src/test/java/org/media/GatewayTests/ReviewGatewayTest.java
@@ -1,7 +1,21 @@
 package org.media.GatewayTests;
 
+import org.junit.Test;
+
 /**
  * Created by shantonu on 2/27/17.
  */
 public class ReviewGatewayTest {
+    @Test
+    public void testGetAReviewtFromDB(){
+
+    }
+    @Test
+    public void testGetAllReviewFromDB(){}
+    @Test
+    public void testDeleteAReviewFromDB(){}
+    @Test
+    public void testInsertAReviewFromDB(){}
+    @Test
+    public void testUpdateAReviewFromDB(){}
 }
