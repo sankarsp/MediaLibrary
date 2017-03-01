7888dda958561f56f408d25319fc3c742183a73f
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-01 01:46:16 -0500

    reviewer gateway added

--- src/test/java/org/media/GatewayTests/ReviewerGatewayTest.java
+++ src/test/java/org/media/GatewayTests/ReviewerGatewayTest.java
@@ -1,11 +1,19 @@
 package org.media.GatewayTests;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.media.dal.gateways.ReviewerGateway;
 
 /**
  * Created by shantonu on 2/27/17.
  */
 public class ReviewerGatewayTest {
+    private ReviewerGateway aReviewerGateway;
+    @Before
+    public void init(){
+        aReviewerGateway = new ReviewerGateway();
+    }
+
     @Test
     public void testGetAReviewerFromDB(){
 
