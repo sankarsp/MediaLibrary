ccadf22ab571401880ec9c7a3287b00707042b04
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-28 23:56:29 -0500

    recording gateway test method

--- src/test/java/org/media/GatewayTests/RecordingGatewayTest.java
+++ src/test/java/org/media/GatewayTests/RecordingGatewayTest.java
@@ -1,7 +1,21 @@
 package org.media.GatewayTests;
 
+import org.junit.Test;
+
 /**
  * Created by shantonu on 2/26/17.
  */
 public class RecordingGatewayTest {
+    @Test
+    public void testGetARecordingtFromDB(){
+
+    }
+    @Test
+    public void testGetAllRecordingFromDB(){}
+    @Test
+    public void testDeleteARecordingFromDB(){}
+    @Test
+    public void testInsertARecordingFromDB(){}
+    @Test
+    public void testUpdateARecordingFromDB(){}
 }
