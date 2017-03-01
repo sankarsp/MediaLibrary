e4712edf31d51545840f2b410787e415436df295
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-01 01:48:55 -0500

    review  gateway gateway added

--- src/test/java/org/media/GatewayTests/ReviewGatewayTest.java
+++ src/test/java/org/media/GatewayTests/ReviewGatewayTest.java
@@ -1,11 +1,19 @@
 package org.media.GatewayTests;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.media.dal.gateways.ReviewGateway;
 
 /**
  * Created by shantonu on 2/27/17.
  */
 public class ReviewGatewayTest {
+
+    private ReviewGateway aReviewGateway;
+    @Before
+    public void init(){
+        aReviewGateway = new ReviewGateway();
+    }
     @Test
     public void testGetAReviewFromDB(){
 
