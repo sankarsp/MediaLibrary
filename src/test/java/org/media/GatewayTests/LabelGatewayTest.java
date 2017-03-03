43972679d33187b8e889cb1489850deb4b9c6ccb
sarkershantonu <shantonu_oxford@yahoo.com>
2017-03-02 23:58:51 -0500

    label gateway all

--- src/test/java/org/media/GatewayTests/LabelGatewayTest.java
+++ src/test/java/org/media/GatewayTests/LabelGatewayTest.java
@@ -3,6 +3,13 @@ package org.media.GatewayTests;
 import org.junit.Before;
 import org.junit.Test;
 import org.media.dal.gateways.LabelGateway;
+import org.media.model.Genre;
+import org.media.model.Label;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -17,7 +24,13 @@ public class LabelGatewayTest {
 
     @Test
     public void testGetALabelFromDB(){
-
+        List<Label> allGenre = aLabelGateway.viewAll();
+        assertTrue(allGenre.size()>0);
+        for(Label aLabel : allGenre){
+            Label found = aLabelGateway.view(aLabel.getID());
+            assertEquals(found.getID(), aLabel.getID());
+            assertEquals(found.getNAME(), aLabel.getNAME());
+        }
     }
     @Test
     public void testGetAllLabelFromDB(){}
